37f6e4e7493c1fcf38ca4542d0a8126638cf5240
Vidhi Kamdar <25697006+vdkamdar@users.noreply.github.com>
2018-04-05 18:01:51 -0400

    Create RestoreI.java

---
+++ genericCheckpointing/src/genericCheckpointing/server/RestoreI.java
@@ -0,0 +1,9 @@
+package genericCheckpointing.server;
+
+import genericCheckpointing.util.MyAllTypesFirst;
+import genericCheckpointing.util.MyAllTypesSecond;
+import genericCheckpointing.util.SerializableObject;
+
+public interface RestoreI extends StoreRestoreI{
+	SerializableObject readObj(String wireFormat);
+}
